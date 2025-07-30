6488368e7e2f318a5304b6949a54470478a71677
Umair Saeed <umairsaeed320@gmail.com>
2025-07-30 02:14:43 -0700

    Update serializer.py

--- user/serializer.py
+++ user/serializer.py
@@ -10,7 +10,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'name', 'roll_number', 'age', 'profile_picture')
+        fields = ('username', 'email', 'password', 'name', 'roll_number', 'age', 'profile_picture','is_teacher')
 
     def create(self, validated_data):
         password = validated_data.pop('password')
